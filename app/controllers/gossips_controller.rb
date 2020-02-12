0fe380b8074cbe48f205b2d23ef84161fe147068
nayow <nayow@hotmail.fr>
2020-02-12 14:00:14 +0100

    finished 2.5

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,7 +1,6 @@
 class GossipsController < ApplicationController
 
   def index
-    # Méthode qui récupère tous les potins et les envoie à la view index (index.html.erb) pour affichage
   end
 
   def show
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,9 @@ class Comment < ApplicationRecord
   has_many :comment_of_comments
 
   validates :content, presence: true
+
+  def self.count(gossip_id)
+    return Comment.where('gossip_id' => gossip_id).count
+  end
+
 end
